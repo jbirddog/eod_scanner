1f63f2f9bc000ac405666e294ede9550b94b2138
Jon Herron <jon.herron@yahoo.com>
2023-07-04 20:44:13 -0400

    Ported remaining parser test cases from raku version

--- parser_test.go
+++ parser_test.go
@@ -6,36 +6,115 @@ import (
 )
 
 func TestParseMinimalEODFile(t *testing.T) {
-	rawData := []string{
-		"Symbol,Date,Open,High,Low,Close,Volume",
-		"AACG,30-May-2023,1.5,1.5745,1.48,1.4906,16900",
-	}
-
-	expected := []*EODData{
-		&EODData{
-			Symbol: "AACG",
-			Date:   Day(2023, 5, 30),
-			Open:   1.5,
-			High:   1.5745,
-			Low:    1.48,
-			Close:  1.4906,
-			Volume: 16900,
+	cases := []struct {
+		rawData  []string
+		expected []*EODData
+	}{
+		// single record
+		{
+			[]string{
+				"Symbol,Date,Open,High,Low,Close,Volume",
+				"AACG,30-May-2023,1.5,1.5745,1.48,1.4906,16900",
+			},
+			[]*EODData{
+				&EODData{
+					Symbol: "AACG",
+					Date:   Day(2023, 5, 30),
+					Open:   1.5,
+					High:   1.5745,
+					Low:    1.48,
+					Close:  1.4906,
+					Volume: 16900,
+				},
+			},
+		},
+		// multiple records
+		{
+			[]string{
+				"Symbol,Date,Open,High,Low,Close,Volume",
+				"AACG,30-May-2023,1.5,1.5745,1.48,1.4906,16900",
+				"AAL,30-May-2023,14.44,14.75,14.42,14.62,20424600",
+			},
+			[]*EODData{
+				&EODData{
+					Symbol: "AACG",
+					Date:   Day(2023, 5, 30),
+					Open:   1.5,
+					High:   1.5745,
+					Low:    1.48,
+					Close:  1.4906,
+					Volume: 16900,
+				},
+				&EODData{
+					Symbol: "AAL",
+					Date:   Day(2023, 5, 30),
+					Open:   14.44,
+					High:   14.75,
+					Low:    14.42,
+					Close:  14.62,
+					Volume: 20424600,
+				},
+			},
+		},
+		// tests price with no decimal place
+		{
+			[]string{
+				"Symbol,Date,Open,High,Low,Close,Volume",
+				"VIA,30-Jun-2023,7,7.1846,6.69,6.96,61000",
+			},
+			[]*EODData{
+				&EODData{
+					Symbol: "VIA",
+					Date:   Day(2023, 6, 30),
+					Open:   7.0,
+					High:   7.1846,
+					Low:    6.69,
+					Close:  6.96,
+					Volume: 61000,
+				},
+			},
+		},
+		// invalid input - no header
+		{
+			[]string{
+				"VIA,30-Jun-2023,7,7.1846,6.69,6.96,61000",
+			},
+			nil,
+		},
+		// invalid input - no data
+		{
+			[]string{
+				"Symbol,Date,Open,High,Low,Close,Volume",
+			},
+			nil,
 		},
 	}
 
-	actual, err := ParseEODFile(rawData)
+	for i, c := range cases {
+		actual, err := ParseEODFile(c.rawData)
 
-	if err != nil {
-		t.Fatalf("Got error: %v", err)
-	}
+		if actual == nil {
+			if c.expected != nil {
+				t.Fatalf("[%d] Got nil response, expected %v", i, c.expected)
+			}
 
-	if len(actual) != len(expected) {
-		t.Fatalf("Expected len of %d, got %d", len(expected), len(actual))
-	}
+			if err == nil {
+				t.Fatalf("[%d] Got nil response, expected error", i)
+			}
 
-	for i, a := range actual {
-		if !sameData(a, expected[i]) {
-			t.Fatalf("Expected %v, got %v", expected[i], a)
+			continue
+		}
+
+		if len(actual) != len(c.expected) {
+			t.Fatalf("[%d] Expected len of %d, got %d", i, len(c.expected), len(actual))
+		}
+
+		for j, a := range actual {
+			expected := c.expected[j]
+
+			if !sameData(a, expected) {
+				t.Fatalf("[%d:%d] Expected %v, got %v", i, j, expected, a)
+			}
 		}
 	}
 }
