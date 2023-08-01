dba428e8b172e0ac04d31d7c23bdc418cd607120
Jon Herron <jon.herron@yahoo.com>
2023-08-01 13:44:10 -0400

    Prep for multiple strategies

--- main.go
+++ main.go
@@ -5,55 +5,23 @@ import (
 	"os"
 )
 
-const marketDayCount = 52
-const riskPerTrade = 50000.0 * 0.005
-
 func main() {
 	dataDir := os.Getenv("EOD_DATA_DIR")
 	if dataDir == "" {
 		log.Fatal("Must set environment variable EOD_DATA_DIR")
 	}
 
-	// TODO: move to driver, use channels?
 	currentDay := Day(2023, 8, 1)
-	dates := PreviousMarketDays(currentDay, marketDayCount)
-	// TODO: AMEX, NYSE
-	exchange := "NASDAQ"
-	eodData := make([][]*EODData, marketDayCount)
-
-	for i := len(dates) - 1; i >= 0; i-- {
-		date := dates[i]
-		rawData, err := LoadEODFile(dataDir, exchange, date)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		data, err := ParseEODFile(rawData)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		eodData[i] = data
-	}
-
-	analyzedDataBySymbol := Analyze(eodData)
+	marketDayCount := 52
 	strategies := []*Strategy{
 		&MonthClimb,
 	}
-	// TODO: by strategy
-	symbols := make([]*AnalyzedData, 0, len(analyzedDataBySymbol))
 
-	for _, v := range analyzedDataBySymbol {
-		if v.DataPoints != marketDayCount {
-			continue
-		}
-
-		for _, strategy := range strategies {
-			if strategy.SignalDetected(v) {
-				symbols = append(symbols, v)
-			}
-		}
+	// TODO: pass in via config
+	results, err := Scan(currentDay, marketDayCount, dataDir, strategies)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	PrintReport(symbols, currentDay)
+	PrintReport(results, currentDay)
 }
--- report.go
+++ report.go
@@ -2,33 +2,38 @@ package main
 
 import (
 	"fmt"
-	"sort"
 	"time"
 )
 
-func PrintReport(symbols []*AnalyzedData, currentDay time.Time) {
-	sort.Slice(symbols, func(i, j int) bool {
-		return MonthClimb.SortWeight(symbols[i]) > MonthClimb.SortWeight(symbols[j])
-	})
+// TODO: pass in via config
+const riskPerTrade = 50000.0 * 0.005
 
-	for _, v := range symbols {
-		p := PositionFromAnalyzedData(v, riskPerTrade)
+func PrintReport(results []*ScanResult, currentDay time.Time) {
+	for _, result := range results {
+		result.Sort()
 
-		// TODO: string builder or buffered writer?
-		fmt.Printf("%s %.2f %.0f (%.2f %.2f) (%.2f %.2f) %.2f | %d @ %.2f ~ %.2f > %.2f\n",
-			v.Symbol,
-			v.RSI.Value,
-			v.AvgVolume,
-			v.LastClose(),
-			v.LastChange(),
-			v.MACD.Line,
-			v.MACD.Signal.Value,
-			v.MACD.Trend,
-			p.Shares,
-			p.Entry,
-			p.Capitol,
-			p.StopLoss)
+		for _, v := range result.Detected {
+			p := PositionFromAnalyzedData(v, riskPerTrade)
+
+			// TODO: string builder or buffered writer?
+			fmt.Printf("%s %.2f %.0f (%.2f %.2f) (%.2f %.2f) %.2f | %d @ %.2f ~ %.2f > %.2f\n",
+				v.Symbol,
+				v.RSI.Value,
+				v.AvgVolume,
+				v.LastClose(),
+				v.LastChange(),
+				v.MACD.Line,
+				v.MACD.Signal.Value,
+				v.MACD.Trend,
+				p.Shares,
+				p.Entry,
+				p.Capitol,
+				p.StopLoss)
+		}
+
+		fmt.Printf("Strategy '%s' found %d symbols. %.2f risk per trade\n\n",
+			result.Strategy.Name,
+			len(result.Detected),
+			riskPerTrade)
 	}
-
-	fmt.Printf("Found %d symbols. %.2f risk per trade\n\n", len(symbols), riskPerTrade)
 }
