13868e03b6e3b4e103505109796979c00af565ea
Jon Herron <jon.herron@yahoo.com>
2023-08-02 06:55:38 -0400

    Bump the day

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 		log.Fatal("Must set environment variable EOD_DATA_DIR")
 	}
 
-	currentDay := Day(2023, 8, 1)
+	currentDay := Day(2023, 8, 2)
 	marketDayCount := 52
 	strategies := []*Strategy{
 		&MonthClimb,
