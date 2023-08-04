9151efc424f043d5eb8ee30186836b1674afe3aa
Jon Herron <jon.herron@yahoo.com>
2023-08-04 10:17:57 -0400

    Fix broken test

--- indicators_test.go
+++ indicators_test.go
@@ -6,11 +6,11 @@ import (
 
 func TestRunningAvgFloat64(t *testing.T) {
 	inputs := []float64{10, 3, 20, 5, 30, 7, 40, 9, 50, 11}
-	actual := 0.0
+	var actual float64
 	expected := 18.5
 
 	for i, input := range inputs {
-		actual = runningAvg(actual, float64(i), input)
+		actual = runningAvg(actual, i, input)
 	}
 
 	if !sameFloat(actual, expected) {
