514351b9d6962985fb1eaf595da914dcf61f8ff9
Jon Herron <jon.herron@yahoo.com>
2023-08-13 09:16:26 -0400

    WIP - helper for boilerplate

--- strategy_test.go
+++ strategy_test.go
@@ -2,30 +2,37 @@ package main
 
 import (
 	"testing"
+	"time"
 )
 
 // TODO: move these to own file once a few are built out
-func CRDO_05152023() *AnalyzedData {
-	d := &AnalyzedData{Symbol: "CRDO"}
+func sc1(sym string,
+	d1 time.Time, o1, h1, l1, c1, v1 float64,
+	d2 time.Time, o2, h2, l2, c2, v2 float64,
+	ml, ms float64,
+	r1, r2, r3, r4, r5 float64,
+	s float64,
+) *AnalyzedData {
+	d := &AnalyzedData{Symbol: sym}
 
 	d.EODData = []*EODData{
 		&EODData{
-			Symbol: "CRDO",
-			Date:   Day(2023, 5, 12),
-			Open:   7.95,
-			High:   8.06,
-			Low:    7.82,
-			Close:  7.91,
-			Volume: 537400.0,
+			Symbol: sym,
+			Date:   d1,
+			Open:   o1,
+			High:   h1,
+			Low:    l1,
+			Close:  c1,
+			Volume: v1,
 		},
 		&EODData{
-			Symbol: "CRDO",
-			Date:   Day(2023, 5, 15),
-			Open:   7.99,
-			High:   8.64,
-			Low:    7.96,
-			Close:  8.61,
-			Volume: 1463100.0,
+			Symbol: sym,
+			Date:   d2,
+			Open:   o2,
+			High:   h2,
+			Low:    l2,
+			Close:  c2,
+			Volume: v2,
 		},
 	}
 
@@ -35,134 +42,50 @@ func CRDO_05152023() *AnalyzedData {
 	i.AvgVolume = 1000001.0
 	i.AvgClose = 5.01
 
-	i.MACD.Line = -0.2746
-	i.MACD.Signal.Value = -0.3848
+	i.MACD.Line = ml
+	i.MACD.Signal.Value = ms
 
-	setRSIs(i, 35.48, 41.89, 43.11, 42.62, 55.19)
+	setRSIs(i, r1, r2, r3, r4, r5)
 
-	i.SMA20.Value = 8.09
+	i.SMA20.Value = s
 
 	return d
+}
+
+func CRDO_05152023() *AnalyzedData {
+	return sc1("CRDO",
+		Day(2023, 5, 12), 7.95, 8.06, 7.82, 7.91, 537400.0,
+		Day(2023, 5, 15), 7.99, 8.64, 7.96, 8.61, 1463100.0,
+		-0.2746, -0.3848,
+		35.48, 41.89, 43.11, 42.62, 55.19,
+		8.09)
 }
 
 func RIOT_01052023() *AnalyzedData {
-	d := &AnalyzedData{Symbol: "RIOT"}
-
-	d.EODData = []*EODData{
-		&EODData{
-			Symbol: "RIOT",
-			Date:   Day(2023, 1, 4),
-			Open:   3.44,
-			High:   3.95,
-			Low:    3.38,
-			Close:  3.88,
-			Volume: 12325300.0,
-		},
-		&EODData{
-			Symbol: "RIOT",
-			Date:   Day(2023, 1, 5),
-			Open:   3.86,
-			High:   4.28,
-			Low:    3.70,
-			Close:  4.22,
-			Volume: 14097000.0,
-		},
-	}
-
-	i := &d.Indicators
-	i.Init()
-
-	i.AvgVolume = 1000001.0
-	i.AvgClose = 5.01
-
-	i.MACD.Line = -0.2691
-	i.MACD.Signal.Value = -0.3502
-
-	setRSIs(i, 31.64, 31.17, 30.84, 46.35, 53.79)
-
-	i.SMA20.Value = 3.85
-
-	return d
+	return sc1("RIOT",
+		Day(2023, 1, 4), 3.44, 3.95, 3.38, 3.88, 12325300.0,
+		Day(2023, 1, 5), 3.86, 4.28, 3.70, 4.22, 14097000.0,
+		-0.2691, -0.3502,
+		31.64, 31.17, 30.84, 46.35, 53.79,
+		3.85)
 }
 
 func RIOT_06272023() *AnalyzedData {
-	d := &AnalyzedData{Symbol: "RIOT"}
-
-	d.EODData = []*EODData{
-		&EODData{
-			Symbol: "RIOT",
-			Date:   Day(2023, 6, 26),
-			Open:   11.49,
-			High:   12.18,
-			Low:    10.72,
-			Close:  10.77,
-			Volume: 22037414.0,
-		},
-		&EODData{
-			Symbol: "RIOT",
-			Date:   Day(2023, 6, 27),
-			Open:   11.07,
-			High:   11.71,
-			Low:    10.88,
-			Close:  11.65,
-			Volume: 25685628.0,
-		},
-	}
-
-	i := &d.Indicators
-	i.Init()
-
-	i.AvgVolume = 1000001.0
-	i.AvgClose = 5.01
-
-	i.MACD.Line = 0.0572
-	i.MACD.Signal.Value = -0.0364
-
-	setRSIs(i, 56.95, 52.84, 55.66, 48.47, 55.09)
-
-	i.SMA20.Value = 11.01
-
-	return d
+	return sc1("RIOT",
+		Day(2023, 6, 26), 11.49, 12.18, 10.72, 10.77, 22037414.0,
+		Day(2023, 6, 27), 11.07, 11.71, 10.88, 11.65, 25685628.0,
+		0.0572, -0.0364,
+		56.95, 52.84, 55.66, 48.47, 55.09,
+		11.01)
 }
 
 func GRPN_06282023() *AnalyzedData {
-	d := &AnalyzedData{Symbol: "GRPN"}
-
-	d.EODData = []*EODData{
-		&EODData{
-			Symbol: "GRPN",
-			Date:   Day(2023, 6, 27),
-			Open:   5.21,
-			High:   5.73,
-			Low:    5.10,
-			Close:  5.56,
-			Volume: 1205454.0,
-		},
-		&EODData{
-			Symbol: "GRPN",
-			Date:   Day(2023, 6, 28),
-			Open:   5.60,
-			High:   6.00,
-			Low:    5.49,
-			Close:  5.93,
-			Volume: 1152836.0,
-		},
-	}
-
-	i := &d.Indicators
-	i.Init()
-
-	i.AvgVolume = 1000001.0
-	i.AvgClose = 5.01
-
-	i.MACD.Line = 0.2382
-	i.MACD.Signal.Value = 0.2085
-
-	setRSIs(i, 57.21, 57.61, 54.05, 57.63, 61.28)
-
-	i.SMA20.Value = 5.31
-
-	return d
+	return sc1("GRPN",
+		Day(2023, 6, 27), 5.21, 5.73, 5.10, 5.56, 1205454.0,
+		Day(2023, 6, 28), 5.60, 6.00, 5.49, 5.93, 1152836.0,
+		0.2382, 0.2085,
+		57.21, 57.61, 54.05, 57.63, 61.28,
+		5.31)
 }
 
 func TestMonthClimb(t *testing.T) {
